945ff68517e2dfa7ffca28b1724ae52237a4aa01
camfilho <camfilho@ufrn.edu.br>
2020-02-09 20:35:22 -0300

    set useEffect to make an api call

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,29 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  // useEffect it schedules everything that is inside of it
+  //to run after rendering
+  //After SearchParams is rendered, useEffect takes effect.
+  // However
+  // Rather than running everyting o type something inside the form input
+  // I need to update just when animal changes
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.erorr);
+  }, [animal, setBreeds, setBreed]);
+
   return (
     <div className="search-params">
       <h1>{location}</h1>
